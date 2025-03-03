14339fda12dcf3473ab69e32ab40b2da7516ad4c
marbhuber <54669828+marbhuber@users.noreply.github.com>
2025-03-03 13:40:39 +0100

    Update bja_comment_auroc_vs_dca.R
    Second revisions

--- bja_comment_auroc_vs_dca.R
+++ bja_comment_auroc_vs_dca.R
@@ -3,7 +3,7 @@
 # the benefit of a decision curve analysis for evaluating the added value of a biomarker for decision-making
 # Comment on Roshanov et al. 2025 (https://doi.org/10.1016/j.bja.2024.10.039)
 #
-# Markus Huber / 4 Feb 2025
+# Markus Huber / 03 March 2025
 #############################################################################################################
 
 #######
@@ -28,6 +28,10 @@ df.auroc         <- c()
 
 N = 35815 # number of patients
 
+# N=1e3
+# one random seed should be set initially prior to the generation of data
+set.seed(1234)
+
 ###########
 # bootstrap
 
@@ -36,27 +40,27 @@ for (myboot in 1:n.boot){
 
   print(myboot)
   
-  ######################
-  # Patient and surgery characteristics only
+  #################################################################################################
+  # Patient and surgery characteristics with eGFR and non-linear eGFR
+  # create one dataset, then fit three logistic regression models with varying degree of complexity
   
-  coefs1 <- c(0.2)
-
-  # prevalence and AUROC from the original study
+  coefs1 <- c(0.2,-0.08,0.08)
   myprev = 0.13
-  myauc  = 0.75
-
-  # seed
-  myseed = sample(1:1e6,1)
-  set.seed(myseed)
+  myauc  = 0.77
   
-  # Patient and surgery characteristics with 6 categories
-  rm(d1,C1,d1a,dd,fit,df.fit)
-  # d1     <- defData(varname = "x1", formula = "1/6;1/6;1/6;1/6;1/6;1/6",variance = 0, dist = "categorical")
+  rm(d1,d2,d3,C1,d1a,dd)
   d1  <- defData(varname = "x1", formula = 0, variance = 1)
-  C1     <- logisticCoefs(defCovar = d1, coefs = coefs1, popPrev = myprev, auc=myauc)
-  d1a    <- defData(d1, varname = "y",formula = "t(..C1) %*% c(1, x1)",dist = "binary", link = "logit")
-  dd     <- genData(N, d1a) %>% as.data.frame() #%>% mutate(x1=factor(x1))
-  fit    <- glm(y ~ x1, data = dd,family=binomial()) # logistic regression
+  d2  <- defData(d1,varname = "x2", formula = 0, variance = 1)
+  d3  <- defData(d2,varname = "x3", formula = "x2*x2")
+  C1  <- logisticCoefs(defCovar = d3, coefs = coefs1, popPrev = myprev, auc=myauc)
+  d1a <- defData(d3, varname = "y",formula = "t(..C1) %*% c(1, x1, x2,x3)",dist = "binary", link = "logit")
+  dd  <- genData(N, d1a) %>% as.data.frame()
+  
+  ############################################
+  # model: patient and surgery characteristics
+
+  rm(fit,df.fit)
+  fit    <- glm(y ~ x1, data = dd,family=binomial())
   df.fit <- data.frame(y=fit$y,prob = fit$fitted.values)
 
   df.auroc <- rbind(df.auroc,data.frame(
@@ -65,63 +69,30 @@ for (myboot in 1:n.boot){
     auc  = as.numeric(pROC::auc(pROC::roc(df.fit$y,df.fit$prob)))
     ))
   
-  dca(y ~ prob, df.fit, thresholds = seq(0, 1, by = 0.01))
-  
-  mydca0       <- dca(y ~ prob, df.fit, thresholds = seq(0, 1, by = 0.01))
+  mydca0       <- dca(y ~ prob, df.fit, thresholds = seq(0, 1, by = 0.02))
   df.dca.treat <- rbind(df.dca.treat,mutate(filter(mydca0$dca,variable!="prob")))
 
-  ###########################
-  # Patient and surgery characteristics with eGFR
+  ######################################################
+  # model: patient and surgery characteristics with eGFR
   
-  set.seed(myseed)
-  coefs1 <- c(0.2,-0.2)
-  myauc  = 0.77
-  
-  rm(d1,d2,C1,d1a,dd,fit,df.fit)
-  # d1     <- defData(varname = "x1", formula = "1/6;1/6;1/6;1/6;1/6;1/6",variance = 0, dist = "categorical")
-  d1  <- defData(varname = "x1", formula = 0, variance = 1)
-  d2     <- defData(d1,varname = "x2", formula = 0, variance = 1)
-  C1     <- logisticCoefs(defCovar = d2, coefs = coefs1, popPrev = myprev, auc=myauc)
-  d1a    <- defData(d2, varname = "y",
-               formula = "t(..C1) %*% c(1, x1, x2)",
-               dist = "binary", link = "logit")
-  dd     <- genData(N, d1a) %>% as.data.frame() #%>% mutate(x1=factor(x1))
+  rm(fit,df.fit)
   fit    <- glm(y ~ x1+x2, data = dd,family=binomial())
   df.fit <- data.frame(y=fit$y,prob = fit$fitted.values)
   
-  #ggeffects::ggemmeans(fit,terms = c("x1")) %>% plot()
-  #ggeffects::ggemmeans(fit,terms = c("x2")) %>% plot()
-  
-  mydca1 <- dca(y ~ prob, df.fit, thresholds = seq(0, 1, by = 0.01))
+  mydca1 <- dca(y ~ prob, df.fit, thresholds = seq(0, 1, by = 0.02))
   df.auroc <- rbind(df.auroc,data.frame(
     boot = myboot,
-    type = "x2",
+    type = "x1+x2",
     auc  = as.numeric(pROC::auc(pROC::roc(df.fit$y,df.fit$prob)))))
   
-  ###############################################
-  # Patient and surgery characteristics with eGFR and non-linear eGFR
+  ##########################################################################
+  # model: patient and surgery characteristics with eGFR and non-linear eGFR
   
-  set.seed(myseed)
-  coefs1 <- c(0.2,-0.2,0.03)
-  myauc  = 0.77
-
-  rm(d1,d2,d3,C1,d1a,dd,fit,df.fit)
-  # d1  <- defData(varname = "x1", formula = "1/6;1/6;1/6;1/6;1/6;1/6",variance = 0, dist = "categorical")
-  d1  <- defData(varname = "x1", formula = 0, variance = 1)
-  d2  <- defData(d1,varname = "x2", formula = 0, variance = 1)
-  d3  <- defData(d2,varname = "x3", formula = "x2*x2")
-  C1  <- logisticCoefs(defCovar = d3, coefs = coefs1, popPrev = myprev, auc=myauc)
-  d1a <- defData(d3, varname = "y",
-               formula = "t(..C1) %*% c(1, x1, x2,x3)",
-               dist = "binary", link = "logit")
-  dd  <- genData(N, d1a) %>% as.data.frame() #%>% mutate(x1=factor(x1))
+  rm(fit,df.fit)
   fit <- glm(y ~ x1+x2+I(x2^2), data = dd,family=binomial())
   df.fit <- data.frame(y=fit$y,prob = fit$fitted.values)
-  mydca2 <- dca(y ~ prob, df.fit, thresholds = seq(0, 1, by = 0.01))
+  mydca2 <- dca(y ~ prob, df.fit, thresholds = seq(0, 1, by = 0.02))
 
-  #ggeffects::ggemmeans(fit,terms = c("x1")) %>% plot()
-  #ggeffects::ggemmeans(fit,terms = c("x2")) %>% plot()
-  
   df.auroc <- rbind(df.auroc,data.frame(
   boot = myboot,
   type = "x1+x2+I(x2)",
@@ -182,7 +153,6 @@ for (myboot in 1:n.boot){
     boot = myboot
   )
 
-
   df.relative <- rbind(df.relative,relative)
 
 }
@@ -191,50 +161,85 @@ for (myboot in 1:n.boot){
 # FIGURE
 ########
 
-# what are the aucs?
-auroc.str <- df.auroc %>% 
-  group_by(type) %>% 
-  summarise(
-    mymean  = mean(auc,na.rm=T) %>% round(2),
-    mylower = quantile(auc,c(0.025),na.rm=T) %>% round(2),
-    myupper = quantile(auc,c(1-0.025),na.rm=T) %>% round(2),
+# AUROC - could you group_by, but for following the derivations more easily without group_by
+
+summary.auroc <- rbind(
+  data.frame(
+    type       = "x1",
+    auroc.mean = filter(df.auroc,type=="x1") %>% na.omit() %>% pull(auc) %>% mean(),
+    auroc.se   = sd(filter(df.auroc,type=="x1") %>% na.omit() %>% pull(auc))/sqrt(n.boot)
+  ),
+  data.frame(
+    type       = "x1+x2",
+    auroc.mean = filter(df.auroc,type=="x1+x2") %>% na.omit() %>% pull(auc) %>% mean(),
+    auroc.se   = sd(filter(df.auroc,type=="x1+x2") %>% na.omit() %>% pull(auc))/sqrt(n.boot)
+  ),
+  data.frame(
+    type       = "x1+x2+I(x2)",
+    auroc.mean = filter(df.auroc,type=="x1+x2+I(x2)") %>% na.omit() %>% pull(auc) %>% mean(),
+    auroc.se   = sd(filter(df.auroc,type=="x1+x2+I(x2)") %>% na.omit() %>% pull(auc))/sqrt(n.boot)
+  )) %>% 
+  mutate(
+    auroc.lower = auroc.mean-1.96*auroc.se,
+    auroc.upper = auroc.mean+1.96*auroc.se
   ) %>% 
   mutate(mystr = paste0(
-    format(round(mymean,2),nsmall = 2),
-    " (",
-    format(round(mylower,2),nsmall = 2),
-    " - ",
-    format(round(myupper,2),nsmall = 2),
-    ")"))
+    format(round(auroc.mean,2),nsmall = 2)))
 
 # DCA
 
-fig.dca.model <- df.dca %>% mutate(net_benefit=1000*net_benefit) %>% 
-  group_by(threshold,type) %>% 
-  summarise(
-    mymean  = mean(net_benefit,na.rm=T),
-    mylower = quantile(net_benefit,c(0.025),na.rm=T),
-    myupper = quantile(net_benefit,c(1-0.025),na.rm=T)
-  ) %>% 
+df.dca.scaled = df.dca %>% mutate(net_benefit = 1000*net_benefit)
+summary.dca <- data.frame()
+
+for (mythres in unique(df.dca.scaled$threshold)){
+  summary.dca <- rbind(summary.dca,rbind(
+  data.frame(
+    threshold  = mythres,
+    type       = "x1",
+    mymean     = filter(df.dca.scaled,type=="x1" & threshold==mythres) %>% na.omit() %>% pull(net_benefit) %>% mean(),
+    dca.se     = sd(filter(df.dca.scaled,type=="x1" & threshold==mythres) %>% na.omit() %>% pull(net_benefit))/sqrt(n.boot)
+  ),
+  data.frame(
+    threshold  = mythres,
+    type       = "x1+x2",
+    mymean     = filter(df.dca.scaled,type=="x1+x2" & threshold==mythres) %>% na.omit() %>% pull(net_benefit) %>% mean(),
+    dca.se     = sd(filter(df.dca.scaled,type=="x1+x2" & threshold==mythres) %>% na.omit() %>% pull(net_benefit))/sqrt(n.boot)
+  ),
+  data.frame(
+    threshold  = mythres,
+    type       = "x1+x2+I(x2)",
+    mymean     = filter(df.dca.scaled,type=="x1+x2+I(x2)" & threshold==mythres) %>% na.omit() %>% pull(net_benefit) %>% mean(),
+    dca.se     = sd(filter(df.dca.scaled,type=="x1+x2+I(x2)" & threshold==mythres) %>% na.omit() %>% pull(net_benefit))/sqrt(n.boot)
+  )) %>% 
+  mutate(
+    mylower = mymean-1.96*dca.se,
+    myupper = mymean+1.96*dca.se
+  ) %>% select(-dca.se)) 
+}
+
+fig.dca.model <- summary.dca %>% 
   mutate(type = case_when(
-    type=="x1"~paste0("Patient and surgery characteristics\nAUROC: ",auroc.str$mystr[1]),
-    type=="x1+x2"~paste0("Patient and surgery characteristics + eGFR\nAUROC: ",auroc.str$mystr[2]),
-    type=="x1+x2+I(x2)"~paste0("Patient and surgery characteristics + eGFR + eGFR^2\nAUROC: ",auroc.str$mystr[3])))
+    type=="x1"~paste0("Patient and surgery characteristics\nAUROC: ",summary.auroc$mystr[1]),
+    type=="x1+x2"~paste0("Patient and surgery characteristics + eGFR\nAUROC: ",summary.auroc$mystr[2]),
+    type=="x1+x2+I(x2)"~paste0("Patient and surgery characteristics + eGFR + eGFR^2\nAUROC: ",summary.auroc$mystr[3]))) %>% 
+  select(threshold,mymean,mylower,myupper,type)
 fig.dca.treat <- df.dca.treat %>% mutate(net_benefit=1000*net_benefit) %>% 
   group_by(threshold,label) %>% 
   summarise(
     mymean  = mean(net_benefit,na.rm=T),
-    mylower = mean(net_benefit,na.rm=T),#quantile(net_benefit,c(0.025),na.rm=T),
-    myupper = mean(net_benefit,na.rm=T)#quantile(net_benefit,c(1-0.025),na.rm=T)
+    mylower = mean(net_benefit,na.rm=T),
+    myupper = mean(net_benefit,na.rm=T)
   ) %>% 
-  mutate(type = label)
+  mutate(type = label) %>% 
+  select(-label) %>% 
+  select(threshold,mymean,mylower,myupper,type)
 
 figA <- rbind(fig.dca.model,fig.dca.treat) %>% 
   mutate(
     type = factor(type,levels = c("Treat All","Treat None",
-                                  paste0("Patient and surgery characteristics\nAUROC: ",auroc.str$mystr[1]),
-                                  paste0("Patient and surgery characteristics + eGFR\nAUROC: ",auroc.str$mystr[2]),
-                                  paste0("Patient and surgery characteristics + eGFR + eGFR^2\nAUROC: ",auroc.str$mystr[3])))) %>% 
+                                  paste0("Patient and surgery characteristics\nAUROC: ",summary.auroc$mystr[1]),
+                                  paste0("Patient and surgery characteristics + eGFR\nAUROC: ",summary.auroc$mystr[2]),
+                                  paste0("Patient and surgery characteristics + eGFR + eGFR^2\nAUROC: ",summary.auroc$mystr[3])))) %>% 
   ggplot(aes(x=threshold,y=mymean,ymin=mylower,ymax=myupper,color=type,fill=type))+
   geom_ribbon(alpha=0.1,colour=NA)+
   geom_line(size=1)+
@@ -250,7 +255,8 @@ figA <- rbind(fig.dca.model,fig.dca.treat) %>%
   ggsci::scale_color_jama()+
   ggsci::scale_fill_jama()+
   ylab("Net benefit (per 1000 patients)")+
-  guides(color = guide_legend(nrow = 2))
+  guides(color = guide_legend(nrow = 2))+
+  labs(title="A")
 
 #######
 # inlet
@@ -265,8 +271,8 @@ figB <- df.relative %>%
   ) %>% 
   summarise(
     mymean = mean(value,na.rm=T),
-    mylower = quantile(value,c(0.025),na.rm=T),
-    myupper = quantile(value,c(1-0.025),na.rm=T)
+    mylower = mean(value,na.rm=T)-sd(value,na.rm=T),
+    myupper = mean(value,na.rm=T)+sd(value,na.rm=T)
   ) %>% 
   ggplot(aes(x=threshold,y=mymean,ymin=mylower,ymax=myupper,color=name,fill=name))+
   geom_ribbon(alpha=0.1,colour=NA)+
@@ -282,39 +288,34 @@ figB <- df.relative %>%
   )+
   scale_color_manual(values=mycol)+
   scale_fill_manual(values=mycol)+
-  ylab("Increase in net benefit\n(per 1000 patients)")
+  ylab("Increase in net benefit\n(per 1000 patients)")+
+  labs(title="B")
 
-plot.with.inset <-
-  ggdraw() +
+# plot.with.inset <-
+ggdraw() +
   draw_plot(figA) +
   draw_plot(figB, x = 0.4, y = .54, width = .53, height = .37)
-plot.with.inset
 
-ggsave("C:/My Programs KAS/KAS Research/M. Huber/AUROC vs DCA/BJA comment/revisions/figure1_06feb2025.jpeg",dpi=1000,width=10,height=6.5)
+ggsave("C:/My Programs KAS/KAS Research/M. Huber/AUROC vs DCA/BJA comment/revisions2/figure_revisions2_02mar25.jpeg",dpi=1000,width=10,height=6.5)
 
 
-##################
+#########################################
 # values for paper
-##################
+# use monte carlo s.e. and derived 95%-CI
+#########################################
 
-df.dca %>% mutate(net_benefit=1000*net_benefit) %>% 
+df.dca %>%
+  mutate(net_benefit=1000*net_benefit) %>% 
   group_by(threshold,type) %>% 
   summarise(
     mymean  = mean(net_benefit,na.rm=T),
-    mylower = quantile(net_benefit,c(0.025),na.rm=T),
-    myupper = quantile(net_benefit,c(1-0.025),na.rm=T)
+    myse    = sd(net_benefit)/sqrt(n.boot)) %>% 
+  mutate(
+    mylower = mymean-1.96*myse,
+    myupper = mymean+1.96*myse
   ) %>% 
-  filter(type=="x1" & threshold==0.15)
+  filter(type=="x1" & threshold==0.2)
 
-df.dca %>% mutate(net_benefit=1000*net_benefit) %>% 
-  group_by(threshold,type) %>% 
-  summarise(
-    mymean  = mean(net_benefit,na.rm=T),
-    mylower = quantile(net_benefit,c(0.025),na.rm=T),
-    myupper = quantile(net_benefit,c(1-0.025),na.rm=T)
-  ) %>% 
-  filter(type=="x1+x2" & threshold==0.15)
-  
 df.relative %>% 
   pivot_longer(contains("delta")) %>% 
   group_by(
@@ -322,35 +323,24 @@ df.relative %>%
   ) %>% 
   summarise(
     mymean = mean(value,na.rm=T),
-    mylower = quantile(value,c(0.025),na.rm=T),
-    myupper = quantile(value,c(1-0.025),na.rm=T)
+    myse   = sd(value)/sqrt(n.boot)) %>% 
+  mutate(
+    mylower = mymean-1.96*myse,
+    myupper = mymean+1.96*myse
   ) %>% 
-  filter(name=="delta.x2" & threshold==0.15)
+  filter(name=="delta.x2" & threshold==0.2)
 
 
-df.dca %>% mutate(net_benefit=1000*net_benefit) %>% 
-  group_by(threshold,type) %>% 
+df.relative %>% 
+  pivot_longer(contains("delta")) %>% 
+  group_by(
+    threshold,name
+  ) %>% 
   summarise(
-    mymean  = mean(net_benefit,na.rm=T),
-    mylower = quantile(net_benefit,c(0.025),na.rm=T),
-    myupper = quantile(net_benefit,c(1-0.025),na.rm=T)
+    mymean = mean(value,na.rm=T),
+    myse   = sd(value)/sqrt(n.boot)) %>% 
+  mutate(
+    mylower = mymean-1.96*myse,
+    myupper = mymean+1.96*myse
   ) %>% 
-  filter(type=="x1" & threshold==0.40)
-
-df.dca %>% mutate(net_benefit=1000*net_benefit) %>% 
-  group_by(threshold,type) %>% 
-  summarise(
-    mymean  = mean(net_benefit,na.rm=T),
-    mylower = quantile(net_benefit,c(0.025),na.rm=T),
-    myupper = quantile(net_benefit,c(1-0.025),na.rm=T)
-  ) %>% 
-  filter(type=="x1+x2" & threshold==0.40)
-
-df.dca %>% mutate(net_benefit=1000*net_benefit) %>% 
-  group_by(threshold,type) %>% 
-  summarise(
-    mymean  = mean(net_benefit,na.rm=T),
-    mylower = quantile(net_benefit,c(0.025),na.rm=T),
-    myupper = quantile(net_benefit,c(1-0.025),na.rm=T)
-  ) %>% 
-  filter(type=="x1+x2+I(x2)" & threshold==0.40)
+  filter(name=="delta.x3" & threshold==0.2)
